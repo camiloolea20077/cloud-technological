48f385073fa9f09426850de1595e1d9b3139e46d
camiloolea20077 <112830645+camiloolea20077@users.noreply.github.com>
2025-08-26 20:35:35 -0500

    Pagina web

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, HostListener } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  email = 'camiloolea200@gmail.com';
+  title = 'cloud-technological';
+    isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 10;
+  }
+}
